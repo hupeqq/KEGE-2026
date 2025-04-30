4b3c51a4d09e363b0f1c1161b1a57a9b6ee9da93
TRazumikin <timonchess2008@gmai.com>
2025-04-30 19:32:35 +0200

    задача

---
+++ 30-04-25/task-17565.py
@@ -0,0 +1,14 @@
+with open("26_17565 (1).txt") as file:
+    N, S = map(int, file.readline().split())
+    sailors = {}
+    for i in file:
+        sailor = list(map(int, i.split()))
+        sailors[sailor[0]] = (sum(sailor[1:]), sailor[-1])
+sailors2 = sorted(sailors, key=lambda x: (sailors[x], -x), reverse=True)
+passed = sailors2[:S]
+for i in range(len(passed) - 1):
+
+
+
+
+
