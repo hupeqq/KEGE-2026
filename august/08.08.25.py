626f82b87488cb3e8a9551f7bcf2e989b6412bdf
hupeqq <timonchess2008@gmail.com>
2025-08-08 17:45:05 +0200

    задача номера телефонов

---
+++ august/08.08.25.py
@@ -0,0 +1,33 @@
+import re
+
+def Maya1(data):
+    phones = []
+    for i in range(len(data.split())):
+        if data[i] == '+7':
+            phone = data[i] + ' ' + data[i + 1] + ' ' + data[i + 2]
+            phones.append(phone)
+
+
+def Maya2(data):
+    phones = []
+    for line in data.split('  '):
+        if '+7' in line:
+            phones.append(line)
+
+
+def Tim(data):
+    phones = []
+    for i in range(len(data)):
+        if data[i] == "+":
+            phones.append(data[i:i + 18])
+
+def regex(data):
+    pattern = r'\+7 \(...\) ...-..-..'
+    re.findall(pattern, data)
+
+with open('UserData.txt', encoding='utf-8') as file:
+    text = file.read()
+
+
+
+
